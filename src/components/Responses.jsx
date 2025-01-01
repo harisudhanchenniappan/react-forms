c3df9798b0ffb3c29d036394f23dc093218254a5
harisudhanchenniappan <harisudhanchenniappan@gmail.com>
2025-01-01 19:28:44 +0530

    7

--- src/components/Responses.jsx
+++ src/components/Responses.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Responses() {
   const [forms, setForms] = useState([]);
